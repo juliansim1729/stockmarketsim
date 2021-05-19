47d7319427855c324882b408f1e09c68dfe32cac
Julian Sim <54722630+juliansim1729@users.noreply.github.com>
2021-05-19 00:00:11 -0400

    docstrings added
    adding docstrings to gensim, very bare, in need of update once more content added

--- genSimulator/GeneralSimulator.py
+++ genSimulator/GeneralSimulator.py
@@ -2,7 +2,7 @@ class GeneralSimulator:
 
     def __init__(self):
         """
-        Standard init method
+        Standard init method, overall controlling class
 
         Arguments:
         None
